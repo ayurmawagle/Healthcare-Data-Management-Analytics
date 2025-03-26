9e6df233254ae23ecf4c95071cb02ed77c763273
Ayurma Wagle <ayurma.wagle@gmail.com>
2025-03-26 11:52:10 +0000

    Created database and table, and imported csv into tables

---
+++ table_creation.sql
@@ -0,0 +1,112 @@
+CREATE DATABASE health_db;
+
+USE health_db;
+
+CREATE TABLE Patient(
+PatientID INT IDENTITY(1,1) PRIMARY KEY,
+Name VARCHAR(100),
+Age TINYINT,
+Gender VARCHAR(20),
+BloodGroup VARCHAR(5)
+);
+
+CREATE TABLE Admission(
+AdmissionID INT IDENTITY(1,1) PRIMARY KEY,
+AdmissionType VARCHAR(50),
+RoomNumber INT,
+DateOfAdmission DATETIME,
+DischargeDate DATETIME,
+LengthofStay INT
+);
+
+CREATE TABLE MedicalCondition(
+ConditionID INT IDENTITY(1,1) PRIMARY KEY,
+Condition VARCHAR(100)
+);
+
+CREATE TABLE Doctor(
+DoctorID INT IDENTITY(1,1) PRIMARY KEY,
+DoctorName VARCHAR(100)
+);
+
+CREATE TABLE Hospital(
+HospitalID INT IDENTITY(1,1) PRIMARY KEY,
+HospitalName VARCHAR(100)
+);
+
+CREATE TABLE InsuranceProvider(
+InsuranceID INT IDENTITY(1,1) PRIMARY KEY,
+InsuranceName VARCHAR(100)
+);
+
+CREATE TABLE Medication(
+MedicationID INT IDENTITY(1,1) PRIMARY KEY,
+Medication VARCHAR(100)
+);
+
+CREATE TABLE TestResult(
+TestID INT IDENTITY(1,1) PRIMARY KEY,
+Result VARCHAR(100),
+);
+
+CREATE TABLE Billing(
+BillID INT IDENTITY(1,1) PRIMARY KEY,
+BillingAmount MONEY
+);
+
+
+--Importing csv into tables
+
+USE health_db;
+GO
+
+INSERT INTO Patient (Name, Age, Gender, BloodGroup)
+SELECT DISTINCT 
+	Name, 
+	Age, 
+	Gender, 
+	[Blood_Type]
+FROM staging_data;
+
+
+INSERT INTO Admission( AdmissionType, RoomNumber, DateOfAdmission, DischargeDate, LengthofStay)
+SELECT DISTINCT
+	Admission_Type,
+    Room_Number,
+    Date_of_Admission,
+    Discharge_Date,
+    Length_of_Stay
+FROM staging_data;
+
+
+INSERT INTO MedicalCondition(Condition)
+SELECT DISTINCT Medical_Condition
+FROM staging_data;
+
+INSERT INTO Doctor(DoctorName)
+SELECT DISTINCT Doctor
+FROM staging_data;
+
+INSERT INTO Hospital(HospitalName)
+SELECT DISTINCT Hospital
+FROM staging_data;
+
+
+INSERT INTO InsuranceProvider(InsuranceName)
+SELECT DISTINCT Insurance_Provider
+FROM staging_data;
+
+INSERT INTO Medication(Medication)
+SELECT DISTINCT Medication
+FROM staging_data;
+
+
+INSERT INTO TestResult(Result)
+SELECT DISTINCT Test_Results
+FROM staging_data;
+
+
+INSERT INTO Billing(BillingAmount )
+SELECT DISTINCT Billing_Amount 
+FROM staging_data;
+
